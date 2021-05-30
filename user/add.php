318051c50f7ed07817754391dfb735a3e1a310d2
Mahmoud <mahmoudesmail51@gmail.com>
2021-05-30 15:20:56 +0200

    pushed to github

---
+++ user/add.php
@@ -0,0 +1,47 @@
+<div class="container">
+    <div class="row" style="text-align:center;">
+        <h3> Weclome </h3>
+    </div>
+    <div class="row sub_msg">
+        <p>login page</p>
+    </div>
+    
+    <div class="row signup">
+        <div class="row">
+            <h3> Please Login</h3>
+        </div>
+        
+        
+        <!--This is the login form for the user-->
+        <!--The php script for the login functionality is in the login.php and is included at the top of the page-->
+        <form action="" method="post" class=".form-horizontal"> 
+            <!--The global variables that holds all alert messages are echoed here with php-->       
+           <div class="row form-group input_group">
+               <label for="" class="col-sm-2">Email:</label>
+               <div class="col-sm-10">
+                  <input type="text" name="email_login" id="email_login" class="form-control"> 
+                  <span id="error_Email"></span> <!--This span tag with id is used to display the client-side error from the custom validation. Same with the others-->
+               </div>
+           </div>
+           
+           <div class="row form-group input_group">
+               <label for="" class="col-sm-2">Password:</label>
+               <div class="col-sm-10">
+                  <input type="password" name="password_login" id="password_login" class="form-control"> 
+                  <span id="error_Password"></span>
+               </div>
+           </div>
+           
+           <div class="row form-group" style="margin: 0px 10px 20px 10px;">
+               <div class="col-xs-12">
+                  <input type="submit" name="login" id="log_in" class="form-control" value="LOGIN"> 
+               </div>
+           </div>
+            
+        </form>
+        
+       
+        
+    </div>
+    
+</div>
---
+++ user/home.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>CMS APPLICATION - INDEX PAGE</title>
+    
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+<link rel="stylesheet" href="../css/custom.css">
+
+</head>
+
+<script>
+$(document).ready(function(){
+//This code is written so as to always make the currently selected tab to be active
+    
+    $('#myTab a').click(function (e) {
+        e.preventDefault();
+        $(this).tab('show');
+        
+        //If the current tab is not active, remove every errors if they exist on the page.
+        if(("#myTab a") !== $(this)){
+            $("#errorFirstname").html("");
+            $("#errorLastname").html("");
+            $("#errorEmail").html("");
+            $("#errorPassword").html("");
+            $("#error_Email").html("");
+            $("#error_Password").html("");
+            $(".alert-danger").remove();
+            $(".alert").remove();
+            //$(".email_alert").remove();
+        }
+    });
+
+    // store the currently selected tab in the hash value
+    $("ul.nav-tabs > li > a").on("shown.bs.tab", function (e) {
+        var id = $(e.target).attr("href").substr(1);
+        window.location.hash = id;
+    });
+
+    // on load of the page: switch to the currently selected tab
+    var hash = window.location.hash;
+    $('#myTab a[href="' + hash + '"]').tab('show');
+    
+    $("#typed_text").typed({
+        strings: ["This is a system that allows users to create read update and delete information."],
+        typeSpeed: 0
+    });
+    
+    
+});
+
+</script>
+
+<body>
+<nav class="navbar" style="background-color: #19aff5;">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar"> 
+      </button>
+      <a class="navbar-brand home" href="#" >Shopify Courses</a>
+    </div>
+    
+  </div>
+</nav>
+<div class="container">
+<!--We use bootstrap to create tabs on the index.php page-->
+    <ul class="nav nav-tabs" id="myTab">
+       <li><a data-toggle='tab' href="#add">Add Course</a></li>
+       <li><a data-toggle='tab' href="#table">Table of contents</a></li>
+    </ul>
+    
+<!--    Here we add the forms elements of the various tabs-->
+    <div class="tab-content">
+      
+       
+       <div id="add" class="tab-pane fade ">
+           <?php include("add.php") ?>
+       </div>
+       
+       <div id="table" class="tab-pane fade">
+       
+       </div>
+       
+      
+    </div>
+        
+</div> 
+
+
+
+</body>
+</html>
